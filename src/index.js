f583311f29c0811c45d36ac824e3de6725395085
Vladyslav Braiko <vladyslavbar@gmail.com>
2020-04-18 19:12:53 +0300

    save drafts

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import makeMurkup from "./js/update-result-markup";
 import makeFetch from "./js/make-fetch";
 const debounce = require("lodash.debounce");
 
-refs.searchForm.addEventListener("input", debounce(showResult, 500));
+refs.searchForm.addEventListener("input", debounce(showResult, 700));
 
 function showResult(event) {
   event.preventDefault();
--- src/js/update-result-markup.js
+++ src/js/update-result-markup.js
@@ -19,6 +19,9 @@ function makeMurkup(searchResult) {
   if (searchResult.length > 10) {
     PNotify.notice({
       title: "Too many matches found. Please, enter a more specific query",
+      delay: 1500,
+      maxOpen: 1,
+      maxStrategy: "close",
     });
     return;
   }
@@ -26,6 +29,9 @@ function makeMurkup(searchResult) {
   if (searchResult.status === 404) {
     PNotify.error({
       title: "Sorry :( Such country does not exist.",
+      delay: 2000,
+      maxOpen: 1,
+      maxStrategy: "close",
     });
     return;
   }
